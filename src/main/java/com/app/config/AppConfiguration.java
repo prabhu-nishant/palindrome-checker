68311169911ee15663f38ea7a1225ec571ae9e33
prabhu-nishant <prabhu.nishant@gmail.com>
2023-01-12 23:05:33 +0000

    Organzing imports and reformatting the code

--- src/main/java/com/app/config/AppConfiguration.java
+++ src/main/java/com/app/config/AppConfiguration.java
@@ -1,10 +1,12 @@
 package com.app.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EntityScan("com.app.model")
 @EnableJpaRepositories("com.app.repository")
 @EnableCaching
 public class AppConfiguration {
